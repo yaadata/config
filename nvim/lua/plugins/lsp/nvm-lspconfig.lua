474b971396eb66db12fd524b0ac1cbdd1598acce
Ydot19 <abdalhalim.yaadata@gmail.com>
2024-06-15 20:20:50 -0500

    rust: auto import

--- nvim/lua/plugins/lsp/nvm-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvm-lspconfig.lua
@@ -139,7 +139,13 @@ local opts = { -- LSP Configuration & Plugins
                 buildFlags = { "-tags=integration,unit,endtoendtest,smoke" }
             },
             -- pyright = {},
-            -- rust_analyzer = {},
+            rust_analyzer = {
+                completion = {
+                    autoimport = {
+                        enable = true,
+                    },
+                },
+            },
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
             --
             -- Some languages (like typescript) have entire language plugins that can be useful:
