6939ae3e2a49441887ba83dac7a823b965029cf2
Ydot19 <abdalhalim.yaadata@gmail.com>
2024-10-02 21:57:10 -0500

    chore: change terminal to be a float

--- nvim/lua/plugins/extras/terminal.lua
+++ nvim/lua/plugins/extras/terminal.lua
@@ -1,12 +1,14 @@
 local opts = {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function(opts)
-        require('toggleterm').setup({
-            direction = 'float',
-            open_mapping = [[<c-\>]],
-        })
-    end
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function(opts)
+    require('toggleterm').setup {
+      direction = 'float',
+      open_mapping = [[<c-\>]],
+    }
+
+    vim.keymap.set('n', '<leader>wt', ':ToggleTerm<enter>', { desc = 'toggle terminal' })
+  end,
 }
 
 return opts
