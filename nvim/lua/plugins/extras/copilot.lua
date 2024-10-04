2c632cca7081fcbaf35aae6239f1f208d564db62
Ydot19 <abdalhalim.yaadata@gmail.com>
2024-10-03 21:54:08 -0500

    chore: disable copilot by default

--- nvim/lua/plugins/extras/copilot.lua
+++ nvim/lua/plugins/extras/copilot.lua
@@ -1,21 +1,24 @@
 local opts = {
-    "zbirenbaum/copilot.lua",
-    config = function()
-        require('copilot').setup({
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-            filetypes = {
-                yaml = true,
-                golang = true,
-                rust = true,
-                python = true,
-                javascript = true,
-                typescript = true,
-                c = true,
-                cpp = true,
-            },
-        })
-    end
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        yaml = true,
+        golang = true,
+        rust = true,
+        python = true,
+        javascript = true,
+        typescript = true,
+        c = true,
+        cpp = true,
+      },
+    }
+    vim.cmd 'Copilot disable'
+  end,
 }
 
 return opts
