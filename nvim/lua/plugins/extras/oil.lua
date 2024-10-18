33b0e612fa14d9e278fb2f427febd7fe58e437e2
Yadi Abdalhalim <yadi.abdalhalim@chime.com>
2024-10-18 05:28:25 -0500

    enhance: improve buffer close and quitting

--- nvim/lua/plugins/extras/oil.lua
+++ nvim/lua/plugins/extras/oil.lua
@@ -3,6 +3,10 @@ local opts = {
   opts = {},
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons', { 'echasnovski/mini.icons', opts = {} } },
+  config = function()
+    require('oil').setup()
+    vim.keymap.set('n', '<leader>fo', '<CMD>Oil<CR>', { desc = 'Open Oil for current directory' })
+  end,
 }
 
 return opts
