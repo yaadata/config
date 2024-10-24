a747413e6c7223472bb8b1792e9533b49d02852f
Yadi Abdalhalim <yadi.abdalhalim@chime.com>
2024-10-23 21:59:46 -0700

    chore: fix commit for mini.surround

--- nvim/lua/plugins/extras/mini-surround.lua
+++ nvim/lua/plugins/extras/mini-surround.lua
@@ -1,20 +1,20 @@
 local opts = {
-    'echasnovski/mini.surround',
-    opts = {},
-    version = '*',
-    config = function()
-        require("mini.surround").setup({
-            mappings = {
-                add = '<leader>msa',        -- Add mini surrounding in Normal / Visual
-                delete = '<leader>msd',     -- Delete mini surrounding in Normal / Visual
-                find = '<leader>msf',       -- Find mini surrounding to the right in Normal / Visual
-                find_left = '<leader>msF',  -- Find mini surrounding to the left in Normal / Visual
-                highlight = '<leader>msh',  -- Highlight mini surrounding in Normal / Visual
-                replace = '<leader>msr',    -- replace surrounding
-                update_n_lines = '<leader>msn', -- Update surrounding for N lines
-            }
-        })
-    end
+  'echasnovski/mini.surround',
+  opts = {},
+  commit = '48a9795c9d352c771e1ab5dedab6063c0a2df037',
+  config = function()
+    require('mini.surround').setup {
+      mappings = {
+        add = '<leader>msa', -- Add mini surrounding in Normal / Visual
+        delete = '<leader>msd', -- Delete mini surrounding in Normal / Visual
+        find = '<leader>msf', -- Find mini surrounding to the right in Normal / Visual
+        find_left = '<leader>msF', -- Find mini surrounding to the left in Normal / Visual
+        highlight = '<leader>msh', -- Highlight mini surrounding in Normal / Visual
+        replace = '<leader>msr', -- replace surrounding
+        update_n_lines = '<leader>msn', -- Update surrounding for N lines
+      },
+    }
+  end,
 }
 
 return opts
