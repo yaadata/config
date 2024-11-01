c15d0fe90341da97e6dee5497b1adde9e8de927c
Yadi Abdalhalim <yadi.abdalhalim@chime.com>
2024-11-01 14:48:38 -0500

    chore: update keymaps for telescope and others

--- nvim/lua/plugins/extras/which-key.lua
+++ nvim/lua/plugins/extras/which-key.lua
@@ -14,6 +14,7 @@ local opts = { -- Useful plugin to show you pending keybinds.
       ['<leader>sm'] = { name = '[M]ulti-cursor', _ = 'which_key_ignore' },
       ['<leader>sq'] = { name = '[Q]uickfix', _ = 'which_key_ignore' },
       ['<leader>st'] = { name = '[T]odo', _ = 'which_key_ignore' },
+      ['<leader>sc'] = { name = '[C]ommands', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       ['<leader>ws'] = { name = '[s]ession', _ = 'which_key_ignore' },
       ['<leader>wm'] = { name = '[m]anagement', _ = 'which_key_ignore' },
@@ -31,6 +32,7 @@ local opts = { -- Useful plugin to show you pending keybinds.
       ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
       ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
       ['<leader>ba'] = { name = '[a]ll [b]uffers', _ = 'which_key_ignore' },
+      ['<leader>by'] = { name = '[y]ank', _ = 'which_key_ignore' },
       ['<leader>v'] = { name = '[v]isual', _ = 'which_key_ignore' },
       ['<leader>vt'] = { name = '[t]reesitter', _ = 'which_key_ignore' },
     }
