889af8fe6930b33691c8017166400df2439e321b
Yadi Abdalhalim <yadi.abdalhalim@chime.com>
2024-11-05 07:39:26 -0600

    enhance: move golang modal to the bottom of the screen

--- nvim/lua/plugins/extras/golang.lua
+++ nvim/lua/plugins/extras/golang.lua
@@ -25,6 +25,13 @@ local opts = {
         },
       },
       run_in_floaterm = true,
+      floaterm = { -- position
+        posititon = 'bottom', -- one of {`top`, `bottom`, `left`, `right`, `center`, `auto`}
+        height = 0.25, -- height of float window if not auto
+        title_colors = 'monokai', -- default to nord, one of {'nord', 'tokyo', 'dracula', 'rainbow', 'solarized ', 'monokai'}
+        -- can also set to a list of colors to define colors to choose from
+        -- e.g {'#D8DEE9', '#5E81AC', '#88C0D0', '#EBCB8B', '#A3BE8C', '#B48EAD'}
+      },
     }
   end,
 }
