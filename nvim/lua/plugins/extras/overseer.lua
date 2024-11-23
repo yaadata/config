894d17f05869ba476e2830ba9ada9e530abe64bf
Ydot19 <abdalhalim.yaadata@gmail.com>
2024-11-23 03:27:27 -0600

    chore: update keymaps for overseer

--- nvim/lua/plugins/extras/overseer.lua
+++ nvim/lua/plugins/extras/overseer.lua
@@ -15,10 +15,9 @@ local opts = {
         direction = 'left',
       }
     end, { desc = '[T]oggle Task Runs' })
-    vim.keymap.set({ 'n' }, '<leader>pr', '@:', { desc = '[R]epeat command' })
-    vim.keymap.set({ 'n' }, '<leader>pa', '<cmd>OverseerTaskAction<CR>', { desc = 'Open [A]ctions' })
+    vim.keymap.set({ 'n' }, '<leader>pa', '<cmd>OverseerTaskAction<CR>', { desc = 'Previous [A]ctions' })
     vim.keymap.set({ 'n' }, '<leader>pc', '<cmd>OverseerBuild<CR>', { desc = '[C]reate new task' })
-    vim.keymap.set({ 'n' }, '<leader>po', '<cmd>OverseerRun<CR>', { desc = '[O]pen tasks' })
+    vim.keymap.set({ 'n' }, '<leader>ps', '<cmd>OverseerRun<CR>', { desc = '[S]earch/[S]elect tasks' })
     vim.keymap.set({ 'n' }, '<leader>pp', function()
       local tasks = os.list_tasks { recent_first = true }
       if vim.tbl_isempty(tasks) then
