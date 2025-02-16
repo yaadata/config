1936c1e85c6e9ba82f82d7b78479830bb9d86173
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-02-15 20:09:33 -0600

    chore: upgrade nvim-notify

--- nvim/lua/plugins/extras/notify.lua
+++ nvim/lua/plugins/extras/notify.lua
@@ -1,13 +1,17 @@
 local opts = {
   'rcarriga/nvim-notify',
-  tag = 'v3.14.0',
+  tag = 'v3.15.0',
   opts = {
+    timeout = 400,
     background_colour = '#000000',
+    render = 'compact',
+    stages = 'fade_in_slide_out',
+    top_down = false,
   },
   config = function(_, opts)
     require('notify').setup(opts)
     vim.notify = require 'notify'
-    vim.keymap.set('n', '<leader>sN', '<cmd>Telescope notify<CR>', { desc = '[N]otifications' })
+    vim.keymap.set('n', '<leader>ns', '<cmd>Telescope notify<CR>', { desc = '[S]earch' })
     vim.keymap.set('n', '<leader>nd', function()
       require('notify').dismiss()
     end, { desc = '[d]ismiss all notification' })
