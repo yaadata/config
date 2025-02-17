c195256518cf61f63db4722e7f3e774e29f09daf
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-02-17 13:00:59 -0600

    enhance: improve mini-pairs for closing < brackets

--- nvim/lua/plugins/extras/mini-pairs.lua
+++ nvim/lua/plugins/extras/mini-pairs.lua
@@ -1,9 +1,14 @@
 local opts = {
-    'echasnovski/mini.pairs',
-    version = '*',
-    config = function()
-        require('mini.pairs').setup({})
-    end
+  'echasnovski/mini.pairs',
+  version = '*',
+  config = function()
+    require('mini.pairs').setup {
+      mappings = {
+        ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+        ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+      },
+    }
+  end,
 }
 
 return opts
