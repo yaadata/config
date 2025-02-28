cc2b1cef62c517501bbc04b0aaebe9a616b0aafa
Yadi Abdalhalim <yadi.abdalhalim@chime.com>
2025-02-28 10:14:05 -0600

    enhance: view path to file in status / update keymapping

--- nvim/lua/plugins/extras/lualine.lua
+++ nvim/lua/plugins/extras/lualine.lua
@@ -1,30 +1,37 @@
 local opts = {
 
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-        {
-            'nvim-tree/nvim-web-devicons',
-            enabled = true,
-        },
-        "cbochs/grapple.nvim",
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    {
+      'nvim-tree/nvim-web-devicons',
+      enabled = true,
     },
-    config = function()
-        require('lualine').setup({
-            theme = 'palenight',
-            sections = {
-                lualine_b = {
-                    {
-                        function()
-                            return require("grapple").name_or_index()
-                        end,
-                        cond = function()
-                            return package.loaded["grapple"] and require("grapple").exists()
-                        end
-                    }
-                }
-            }
-        })
-    end
+    'cbochs/grapple.nvim',
+  },
+  config = function()
+    require('lualine').setup {
+      theme = 'palenight',
+      sections = {
+        lualine_a = { 'branch' },
+        lualine_b = {
+          {
+            function()
+              return require('grapple').name_or_index()
+            end,
+            cond = function()
+              return package.loaded['grapple'] and require('grapple').exists()
+            end,
+          },
+        },
+        lualine_c = {
+          { 'filename', path = 1 },
+          'diff',
+        },
+        lualine_y = { 'location' },
+        lualine_z = { 'mode' },
+      },
+    }
+  end,
 }
 
 return opts
