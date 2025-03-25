efef2376c6d05f16cd19c1c62af693c40dfbd858
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-03-25 08:39:37 -0400

    enhance: improve keymaps and alacritty

--- nvim/lua/plugins/extras/telescope.lua
+++ nvim/lua/plugins/extras/telescope.lua
@@ -113,7 +113,7 @@ local opts = { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sl', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = '[S]earch by Live Grep' })
 
     -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>sc', function()
+    vim.keymap.set('n', '<leader>sC', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 0,
