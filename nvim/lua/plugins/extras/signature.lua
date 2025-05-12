732d3af0cebee88989ba0f5c5b18f754615893d9
Yadi Abdalhalim <yadi.abdalhalim@flex.one>
2025-05-11 21:38:33 -0500

    feat: re-add signature lua

---
+++ nvim/lua/plugins/extras/signature.lua
@@ -0,0 +1,22 @@
+local opts = {
+  'ray-x/lsp_signature.nvim',
+  event = 'InsertEnter',
+  commit = 'a4e3c15dfdce8783c074539b1835edae75fa63d5',
+  opts = {
+    bind = true,
+    handler_opts = {
+      border = 'rounded',
+    },
+    hint_prefix = '💡 ', -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+    floating_window = false,
+    floating_window_above_cur_line = false,
+  },
+  config = function(_, opts)
+    require('lsp_signature').setup(opts)
+    vim.keymap.set({ 'n' }, '<Leader>lk', function()
+      vim.lsp.buf.signature_help()
+    end, { silent = true, noremap = true, desc = 'toggle signature' })
+  end,
+}
+
+return opts
