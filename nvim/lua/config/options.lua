8a381c23aa339b876de63582e1d0e9cf3c91811e
Yadi Abdalhalim <yadi.abdalhalim@flex.one>
2025-05-13 15:53:48 -0500

    chore: stop emitting swapfile messages

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -72,6 +72,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Swap file
+vim.opt.swapfile = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
