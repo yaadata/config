ece8c2c75fcf16ed969ff0246ce633889d448a3a
Yaadata Abdalhalim <yabdalhalim@mux.com>
2025-05-31 21:42:32 -0500

    fix: telescope quickfix and treesitter-walk keymaps

--- nvim/lua/plugins/extras/telescope.lua
+++ nvim/lua/plugins/extras/telescope.lua
@@ -70,7 +70,7 @@ local opts = { -- Fuzzy Finder (files, lsp, etc)
           i = {
             ['<C-q>'] = function(prompt_bufnr)
               require('telescope.actions').smart_send_to_qflist(prompt_bufnr)
-              require('telescope.actions').open_qflist(prompt_bufnr)
+              -- require('telescope.actions').open_qflist(prompt_bufnr)
             end,
             ['<C-f>'] = require('telescope.actions').to_fuzzy_refine,
           },
--- nvim/lua/plugins/extras/treesitter-walk.lua
+++ nvim/lua/plugins/extras/treesitter-walk.lua
@@ -27,16 +27,16 @@ local opts = {
   config = function(_, opts)
     require('treewalker').setup(opts)
     -- movement
-    vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
-    vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
-    vim.keymap.set({ 'n', 'v' }, '<C-h>', '<cmd>Treewalker Left<cr>', { silent = true })
-    vim.keymap.set({ 'n', 'v' }, '<C-l>', '<cmd>Treewalker Right<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-k>', '<cmd>Treewalker Up<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-j>', '<cmd>Treewalker Down<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-h>', '<cmd>Treewalker Left<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-l>', '<cmd>Treewalker Right<cr>', { silent = true })
 
     -- swapping
-    vim.keymap.set('n', '<C-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
-    vim.keymap.set('n', '<C-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
-    vim.keymap.set('n', '<C-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
-    vim.keymap.set('n', '<C-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-S-k>', '<cmd>Treewalker SwapUp<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-S-j>', '<cmd>Treewalker SwapDown<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-S-h>', '<cmd>Treewalker SwapLeft<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<M-S-l>', '<cmd>Treewalker SwapRight<cr>', { silent = true })
   end,
 }
 
