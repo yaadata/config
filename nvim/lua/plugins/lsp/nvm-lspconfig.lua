12725cc830aa73baff35da07f4275c88c41e9d9b
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-09-21 17:30:22 -0500

    enhance: add fish lsp, requires brew install fish-lsp

--- nvim/lua/plugins/lsp/nvm-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvm-lspconfig.lua
@@ -221,6 +221,10 @@ local opts = { -- LSP Configuration & Plugins
         },
         cmd = { 'mise', 'x', '--', 'ruby-lsp' },
       },
+      fish_lsp = {
+        root_dir = vim.fn.getcwd(),
+        root_markers = { 'config.fish' },
+      },
     }
 
     -- Ensure the servers and tools above are installed
