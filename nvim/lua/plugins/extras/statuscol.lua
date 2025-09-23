feda8c7ea5175ffcbc40340cc353bb0f688af6b7
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-09-23 10:44:38 -0500

    fix: staline instead of statuscol
    pure lua implementation

--- nvim/lua/plugins/extras/statuscol.lua
+++ nvim/lua/plugins/extras/statuscol.lua
@@ -1,22 +1,14 @@
 local opts = {
-  'luukvbaal/statuscol.nvim',
+  'tamton-aquib/staline.nvim',
   config = function()
-    local builtin = require 'statuscol.builtin'
-    require('statuscol').setup {
-      -- configuration goes here, for example:
-      relculright = true,
-      segments = {
-        { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
-        {
-          sign = { namespace = { 'diagnostic/signs' }, maxwidth = 2, auto = true },
-          click = 'v:lua.ScSa',
-        },
-        { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
-        {
-          sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-          click = 'v:lua.ScSa',
-        },
-      },
+    vim.opt.laststatus = 2
+    vim.opt.showtabline = 2
+    vim.opt.termguicolors = true
+    require('stabline').setup {
+      style = 'slant',
+      bg = '#986fec',
+      fg = 'black',
+      stab_right = '',
     }
   end,
 }
