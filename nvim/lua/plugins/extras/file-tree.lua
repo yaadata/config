9fb56e30a01c9d99bde1121186a5145179780c2f
Yaadata Abdalhalim <yabdalhalim@mux.com>
2025-10-08 09:36:18 -0500

    [nvim/lua] refactor: simplify filetree navigation with fyler

---
+++ nvim/lua/plugins/extras/file-tree.lua
@@ -0,0 +1,38 @@
+local opts = {
+  'A7Lavinraj/fyler.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  version = 'v1.1.0',
+  opts = {
+    icon_provider = 'nvim_web_devicons',
+    mappings = {
+      ['-'] = 'GotoParent',
+      ['_'] = 'SelectSplit',
+    },
+  },
+  keys = {
+    {
+      '<leader>ft',
+      function()
+        require('fyler').toggle {
+          kind = 'split_left_most',
+        }
+      end,
+      desc = 'Open File Tree',
+    },
+    {
+      '<leader>fg',
+      function()
+        require('fyler').toggle {
+          dir = vim.fs.root(0, '.git'),
+          kind = 'split_left_most',
+        }
+      end,
+      desc = 'Open Nearest Git File Tree',
+    },
+  },
+}
+
+return opts
