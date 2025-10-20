f3e965c6191cb30f19e67510a7d97747b16511a6
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2025-10-19 20:08:57 -0400

    refactor(nvim): remove lazygit plugin, use toggle term instead for lg

---
+++ nvim/lua/utils/lazygit_toggle.lua
@@ -0,0 +1,29 @@
+local M = {}
+M.toggle = function()
+  local Terminal = require('toggleterm.terminal').Terminal
+
+  local size = 90
+  local direction = 'float'
+
+  if not term then
+    term = Terminal:new {
+      cmd = 'lazygit',
+      hidden = true,
+      on_exit = function()
+        term = nil
+      end,
+    }
+    if term then
+      term:toggle(size, direction)
+
+      vim.cmd 'set ft=lazygit'
+      vim.keymap.set('t', 'q', function()
+        term:toggle(size, direction)
+      end, { buffer = true })
+    end
+  else
+    term:toggle(size, direction)
+  end
+end
+
+return M
