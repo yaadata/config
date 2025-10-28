bac4b662a69f7906e3ede64b524416709068caae
Yaadata Abdalhalim <yabdalhalim@mux.com>
2025-10-28 14:33:45 -0500

    fix(nvim): default shell fish

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -92,4 +92,4 @@ vim.opt.foldlevelstart = 99
 vim.opt.fillchars = [[eob: ,fold: ,foldopen:▾,foldsep: ,foldclose:▸]]
 vim.opt.foldenable = true
 -- set default shell at all times
-vim.opt.shell = '/opt/homebrew/bin/fish'
+vim.opt.shell = '/bin/zsh'
