9345357c810a724ff9382b54c36cb44b218025ce
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2026-01-18 15:32:14 -0600

    fix(fish): disable background tasks for claude

--- fish/env.fish
+++ fish/env.fish
@@ -4,6 +4,8 @@ set -gx dlv ~/go/bin/dlv
 
 set -gx CLAUDE_CONFIG_DIR ~/.config/claude
 
+set -gx CLAUDE_CODE_DISABLE_BACKGROUND_TASKS true
+
 set -gx XDG_CONFIG_HOME ~/.config
 
 set -gx KUBECONFIG ~/.kube/config
