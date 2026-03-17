e69de66e5afca890093579b3749112fd9c13c5bf
Yadi Abdalhalim <abdalhalim.yaadata@gmail.com>
2026-03-17 10:11:24 -0500

    docs(nvim): nvim dadbod test with codex skills

--- nvim/lua/plugins/extras/dadbod.lua
+++ nvim/lua/plugins/extras/dadbod.lua
@@ -12,7 +12,7 @@ local opts = {
   },
 
   init = function()
-    -- Your DBUI configuration
+    -- Enable Nerd Font icons in DBUI buffers and the connection tree.
     vim.g.db_ui_use_nerd_fonts = 1
   end,
 }
