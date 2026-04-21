c2ae845da80d0115dd6709669d6231b42f443e6d
Yaadata Abdalhalim <yabdalhalim@mux.com>
2026-04-21 09:54:20 -0500

    fix(nvim): restore config causing breaks

--- nvim/lua/plugins/sessions.lua
+++ nvim/lua/plugins/sessions.lua
@@ -15,7 +15,8 @@ local opts = {
 
   opts = {
     git_use_branch_name = true,
-    git_auto_restore_on_branch_change = true,
+    git_auto_restore_on_branch_change = false,
+    lsp_stop_on_restore = true,
     show_auto_restore_notif = true,
     auto_delete_empty_sessions = true,
     purge_after_minutes = 43200,
