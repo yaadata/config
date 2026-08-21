a08e3252c85916f1d82036af2f5b99ee52b26430
Yaadata Abdalhalim <yabdalhalim@mux.com>
2026-08-21 15:59:33 -0500

    fix(nvim): use global statusline

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -2,7 +2,6 @@ local opts = {
   'tamton-aquib/staline.nvim',
   commit = 'd337bc9b343df3328921ef4c3f8ff604102d0201',
   config = function()
-    vim.opt.laststatus = 2
     vim.opt.showtabline = 2
     vim.opt.termguicolors = true
     require('stabline').setup {
