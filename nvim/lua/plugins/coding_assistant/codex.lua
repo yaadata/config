743f4ee9480133cf0040e09e08dc23da03347e27
Yaadata Abdalhalim <yabdalhalim@mux.com>
2026-09-14 15:58:41 -0500

    refactor: lsp key map

--- nvim/lua/plugins/coding_assistant/codex.lua
+++ nvim/lua/plugins/coding_assistant/codex.lua
@@ -14,7 +14,7 @@ local opts = {
   },
   keys = {
     {
-      '<leader>lot',
+      '<leader>wot',
       function()
         require('codex').toggle()
       end,
@@ -30,7 +30,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>loo',
+      '<leader>woo',
       function()
         require('codex').open(true)
       end,
@@ -38,7 +38,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>lof',
+      '<leader>wof',
       function()
         require('codex').focus()
       end,
@@ -46,7 +46,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>lox',
+      '<leader>wox',
       function()
         require('codex').close()
       end,
@@ -54,7 +54,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>los',
+      '<leader>wos',
       function()
         require('codex').send_file()
       end,
@@ -62,7 +62,7 @@ local opts = {
       mode = 'n',
     },
     {
-      '<leader>los',
+      '<leader>wos',
       function()
         require('codex').send_selection()
       end,
@@ -70,7 +70,7 @@ local opts = {
       mode = 'x',
     },
     {
-      '<leader>lom',
+      '<leader>wom',
       function()
         local codex = require 'codex'
         codex.mention_file()
@@ -82,7 +82,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>loM',
+      '<leader>woM',
       function()
         local codex = require 'codex'
         codex.mention_directory()
@@ -96,7 +96,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>loi',
+      '<leader>woi',
       function()
         require('codex').execute_slash_command { command = 'status' }
       end,
@@ -104,7 +104,7 @@ local opts = {
       mode = { 'n', 'v' },
     },
     {
-      '<leader>lor',
+      '<leader>wor',
       function()
         local codex = require 'codex'
         local ok, err = codex.send_selection()
@@ -119,7 +119,7 @@ local opts = {
       mode = { 'v' },
     },
     {
-      '<leader>lor',
+      '<leader>wor',
       function()
         require('codex').resume()
       end,
@@ -127,7 +127,7 @@ local opts = {
       mode = { 'n' },
     },
     {
-      '<leader>loc',
+      '<leader>woc',
       function()
         local codex = require 'codex'
         local ok, err = codex.send_selection()
@@ -142,7 +142,7 @@ local opts = {
       mode = { 'v' },
     },
     {
-      '<leader>loc',
+      '<leader>woc',
       function()
         local codex = require 'codex'
         codex.execute_slash_command { command = 'copy' }
--- nvim/lua/plugins/ide/nvm-lspconfig.lua
+++ nvim/lua/plugins/ide/nvm-lspconfig.lua
@@ -86,37 +86,37 @@ local opts = { -- LSP Configuration & Plugins
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>lls', vim.lsp.buf.document_symbol, '[D]ocument [S]ymbols')
+        map('<leadeer>ls', vim.lsp.buf.document_symbol, '[D]ocument [S]ymbols')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('<leader>llws', vim.lsp.buf.workspace_symbol, '[W]orkspace symbols')
+        map('<leadeer>lws', vim.lsp.buf.workspace_symbol, '[W]orkspace symbols')
 
         -- Add folder to workspace folders list
-        map('<leader>llwa', vim.lsp.buf.add_workspace_folder, '[A]dd [W]orkspace Folder')
+        map('<leadeer>lwa', vim.lsp.buf.add_workspace_folder, '[A]dd [W]orkspace Folder')
 
         -- Remove folder to workspace folders list
-        map('<leader>llwr', vim.lsp.buf.remove_workspace_folder, '[R]emove [W]orkspace Folder')
+        map('<leadeer>lwr', vim.lsp.buf.remove_workspace_folder, '[R]emove [W]orkspace Folder')
 
-        map('<leader>llhi', '<cmd>Telescope hierarchy incoming_calls<cr>', '[I]ncoming Calls')
-        map('<leader>llho', '<cmd>Telescope hierarchy outgoing_calls<cr>', '[O]utgoing Calls')
+        map('<leadeer>lhi', '<cmd>Telescope hierarchy incoming_calls<cr>', '[I]ncoming Calls')
+        map('<leadeer>lho', '<cmd>Telescope hierarchy outgoing_calls<cr>', '[O]utgoing Calls')
 
         -- List workspace folders
-        map('<leader>llwl', vim.lsp.buf.list_workspace_folders, '[L]ist [W]orkspace folders')
+        map('<leadeer>lwl', vim.lsp.buf.list_workspace_folders, '[L]ist [W]orkspace folders')
 
         -- format buffer
-        map('<leader>llf', vim.lsp.buf.format, '[F]ormat')
+        map('<leadeer>lf', vim.lsp.buf.format, '[F]ormat')
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map('<leader>llr', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('<leadeer>lr', vim.lsp.buf.rename, '[R]e[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>lla', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('<leadeer>la', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
         -- Inlay hints
-        map('<leader>lli', function()
+        map('<leadeer>li', function()
           vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
         end, '[C]ode [I]nlay Hints')
 
